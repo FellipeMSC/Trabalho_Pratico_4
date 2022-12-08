f4db977b382b3a3fefe7070e799da0a0f5f7ace2
Lucas Rocha Ferreira <104071470+DraconiicDragon@users.noreply.github.com>
2022-12-08 20:29:16 -0300

    TAD da lista

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "./TAD_CircularDuplamenteEncadeada.cpp"
+
+
+using namespace std;
+using namespace circularDuplamenteEncadeada;
+
+int main() {
+    Lista lista;
+    criaListaVazia(&lista);
+    Item item;
+    item.id = 2;
+    insereItemUltimo(&lista, item);
+
+    item.id = 4;
+    insereItemUltimo(&lista, item);
+
+    item.id = 3;
+    insereItemAposElemento(&lista, item, 2);
+
+    item.id = 1;
+    insereItemPrimeiro(&lista, item);
+
+    retiraItemPorId(&lista, 4);
+    
+    cout << endl << "Normal" << endl;
+    imprimeLista(&lista);
+
+    cout << endl << "Reverso" << endl;
+    imprimeListaReversa(&lista);
+
+    cout << endl << "Anterior do primeiro" << endl;
+    imprimeItem(lista.primeiro->ante->item);
+
+    return 0;
+}
